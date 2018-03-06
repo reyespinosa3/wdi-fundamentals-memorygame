1d2c33d036fd3495412b568ec7e409e59a2e20f3
Rey Espinosa <aztecrey3@gmail.com>
2018-03-05 22:08:13 -0600

    added reset button and score

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+//confirms game is ready
 console.log("Up and Running!");
+
+//set up object variable to store card attributes
 var cards = [
 {
 	rank: "queen",
@@ -21,17 +24,40 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
+//created array to track number of cards flipped
 var cardsInPlay = [];
+
+//created class to identify flipped cards
+var flippedCards = document.getElementsByClassName('flipped');
+
+//variable to track score
+var score = 0;
+
+//function to confirm 2 cards have been flipped
+//generate pop-up for player to see if a match
+//was made or not, add 1 point for a match and
+//update score showing on webpage
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You Found a Match!");
+		score += 1;
+		document.getElementById('score').textContent = score;
 	} else {
 		alert("Sorry, Try Again!");
 	}
 };
+
+//function created to identify which card was
+//flipped, update game board to show card image
+//assign a class to the flipped card, check to
+//see if 2 cards have been flipped(if yes,
+//run checkForMatch function), empty cardsInPlay
+//array and console.log card rank, image & suit
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
 	this.setAttribute('src', cards[cardId].cardImage);
+	this.setAttribute('class', "flipped");
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -42,6 +68,9 @@ var flipCard = function() {
 	}
 };
 
+//function to create 4 cards on game board,
+//set card images to the back of the cards,
+//and create event to listen for click
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
@@ -51,4 +80,17 @@ var createBoard = function() {
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 };
+
+//function created to reset images on game board
+//to the back of the cards, reset score variable
+//and update score to 0 on webpage
+var resetBoard = function() {
+	for (i = 0; i < flippedCards.length; i++) {
+		flippedCards[i].setAttribute('src', "images/back.png");
+		score = 0;
+		document.getElementById('score').textContent = 0;
+	}
+};
+
+//Create board to start game
 createBoard();
